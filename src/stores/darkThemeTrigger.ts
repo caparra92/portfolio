c07e7fab420df1748ef16c108c2123fc327e00ac
Camilo Parra <caparra92@gmail.com>
2024-04-23 14:52:18 -0400

    Input validation complete, missing type definitions

--- src/stores/darkThemeTrigger.ts
+++ src/stores/darkThemeTrigger.ts
@@ -1,10 +1,14 @@
 import { defineStore } from 'pinia'
 
 export const useDarkThemeTrigger = defineStore('darkThemeTrigger', {
-  state: () => ({darkTrigger : false}),
+  state: () => ({
+    darkTrigger : false,
+    mapStyle: "mapbox://styles/mapbox/streets-v12"
+  }),
   actions: {
     changeValue() {
       this.darkTrigger = !this.darkTrigger;
+      this.mapStyle = "mapbox://styles/mapbox/navigation-night-v1"
     }
   }
 });
